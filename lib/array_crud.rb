39e1f58c904a117eae33a9838ab4cb855de1d783
Liz Laffitte <liz.kossakowski@gmail.com>
2019-10-09 00:48:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  the_elements = ['fire']
+  the_elements = ["fire", "water", "earth", "air"]
 end
 
 def add_element_to_end_of_array(array, element)
